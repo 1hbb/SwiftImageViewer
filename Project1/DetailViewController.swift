b9f77d36041318ecfb1fd6b4a8101054c5e4dc84
Harun Burak Bursa <1fobofobi@gmail.com>
2021-01-27 00:17:38 +0300

    Update

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    var selectedImage:String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        title = selectedImage
+        // for making tabbar title little
+        navigationItem.largeTitleDisplayMode = .never
+        if let imageToLoad = selectedImage {
+            image.image = UIImage(named: imageToLoad)
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // this code run when this viewcontroller opened
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
     }
-    */
-
+    
+    // this code run when this viewcontroller closed
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    
 }
--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Storm Viewer"
+        
+        // for making our title big in initial screen. But it effects our sub navigations.
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            print(item)
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        
     }
-
-
+    
+    // this func means how many row in our table view
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    // this method describes how each cell looks like
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath) //definin cell
+        cell.textLabel?.text = pictures[indexPath.row] //index path contains [sectionIndex, rowIndex]
+        return cell
+    }
+    
+    // this method says what happens when row tapped
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // creating viewcontroller from storyboard with StoryboardID
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController{
+            vc.selectedImage = pictures[indexPath.row]
+            
+            // pushing our vc to navigation stack
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+    }
+    
+    
 }
 
